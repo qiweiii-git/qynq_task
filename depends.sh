ef40dddb2788f3a6e3defb5934acf18099703482
qiweiii-git <qiweiii@126.com>
2021-03-12 18:10:03 +0800

    Update depends.sh

--- depends.sh
+++ depends.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 #depends=('u-boot-xlnx' 'git clone https://github.com/Xilinx/u-boot-xlnx.git' 
 #         'linux-Digilent-Dev' 'git clone http://github.com/Digilent/linux-Digilent-Dev')
 depends=('u-boot-xlnx' 'git clone https://gitee.com/qiweiii-gitee/u-boot-xlnx.git' 
