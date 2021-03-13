68ffb1b5d2b45de4f7f73967fa4520f60364f55e
Qiwei Wu <qiweiii@126.com>
2021-03-13 23:09:56 +0800

    Add qynq08_sdboot project

---
+++ project/qynq08_sdboot/UpgradeConfig.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#*****************************************************************************
+# Files
+#*****************************************************************************
+upgradeFiles=('boot.bin' 'BOOT.BIN' 
+              'devicetree' 'devicetree.dtb' 
+              'rootfs' 'uramdisk.image.gz' 
+              'kernel' 'uImage')
