fd136a8870f9212e0a7f1137efa27df606ed8aae
Qiwei Wu <qiweiii@126.com>
2021-05-09 11:06:42 +0800

    Update firmware

--- code/firmware/build/System.tcl
+++ code/firmware/build/System.tcl
@@ -144,6 +144,20 @@ proc create_root_design { parentCell } {
 
   # Create interface ports
   set AXIS_MM2S [ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:axis_rtl:1.0 AXIS_MM2S ]
+  set AXIS_S2MM [ create_bd_intf_port -mode Slave -vlnv xilinx.com:interface:axis_rtl:1.0 AXIS_S2MM ]
+  set_property -dict [ list \
+CONFIG.FREQ_HZ {100000000} \
+CONFIG.HAS_TKEEP {1} \
+CONFIG.HAS_TLAST {1} \
+CONFIG.HAS_TREADY {1} \
+CONFIG.HAS_TSTRB {0} \
+CONFIG.LAYERED_METADATA {undef} \
+CONFIG.PHASE {0.000} \
+CONFIG.TDATA_NUM_BYTES {4} \
+CONFIG.TDEST_WIDTH {0} \
+CONFIG.TID_WIDTH {0} \
+CONFIG.TUSER_WIDTH {1} \
+ ] $AXIS_S2MM
   set DDR [ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:ddrx_rtl:1.0 DDR ]
   set FIXED_IO [ create_bd_intf_port -mode Master -vlnv xilinx.com:display_processing_system7:fixedio_rtl:1.0 FIXED_IO ]
   set REG_CTRL [ create_bd_intf_port -mode Master -vlnv xilinx.com:interface:bram_rtl:1.0 REG_CTRL ]
@@ -154,7 +168,7 @@ CONFIG.MASTER_TYPE {BRAM_CTRL} \
   # Create ports
   set AXIS_ACLK [ create_bd_port -dir I -type clk AXIS_ACLK ]
   set_property -dict [ list \
-CONFIG.ASSOCIATED_BUSIF {AXIS_S2MM:AXIS_MM2S} \
+CONFIG.ASSOCIATED_BUSIF {AXIS_MM2S:AXIS_S2MM} \
 CONFIG.ASSOCIATED_RESET {AXIS_RSTN} \
  ] $AXIS_ACLK
   set AXIS_RSTN [ create_bd_port -dir I -type rst AXIS_RSTN ]
@@ -184,19 +198,19 @@ CONFIG.S01_HAS_DATA_FIFO {2} \
   set_property -dict [ list \
 CONFIG.c_include_mm2s {1} \
 CONFIG.c_include_mm2s_dre {1} \
-CONFIG.c_include_s2mm {0} \
-CONFIG.c_include_s2mm_dre {0} \
+CONFIG.c_include_s2mm {1} \
+CONFIG.c_include_s2mm_dre {1} \
 CONFIG.c_m_axi_mm2s_data_width {64} \
 CONFIG.c_m_axi_s2mm_data_width {64} \
-CONFIG.c_m_axis_mm2s_tdata_width {24} \
-CONFIG.c_mm2s_genlock_mode {0} \
+CONFIG.c_m_axis_mm2s_tdata_width {32} \
+CONFIG.c_mm2s_genlock_mode {1} \
 CONFIG.c_mm2s_linebuffer_depth {2048} \
 CONFIG.c_mm2s_max_burst_length {16} \
 CONFIG.c_num_fstores {4} \
 CONFIG.c_s2mm_genlock_mode {0} \
 CONFIG.c_s2mm_linebuffer_depth {2048} \
 CONFIG.c_s2mm_max_burst_length {16} \
-CONFIG.c_use_s2mm_fsync {2} \
+CONFIG.c_use_s2mm_fsync {0} \
  ] $axi_vdma_0
 
   # Create instance: processing_system7_0, and set properties
@@ -269,10 +283,12 @@ CONFIG.NUM_SI {1} \
   set rst_processing_system7_0_50M [ create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 rst_processing_system7_0_50M ]
 
   # Create interface connections
+  connect_bd_intf_net -intf_net S_AXIS_S2MM_1 [get_bd_intf_ports AXIS_S2MM] [get_bd_intf_pins axi_vdma_0/S_AXIS_S2MM]
   connect_bd_intf_net -intf_net axi_bram_ctrl_0_BRAM_PORTA [get_bd_intf_ports REG_CTRL] [get_bd_intf_pins axi_bram_ctrl_0/BRAM_PORTA]
   connect_bd_intf_net -intf_net axi_interconnect_0_M00_AXI [get_bd_intf_pins axi_interconnect_0/M00_AXI] [get_bd_intf_pins processing_system7_0/S_AXI_HP0]
   connect_bd_intf_net -intf_net axi_vdma_0_M_AXIS_MM2S [get_bd_intf_ports AXIS_MM2S] [get_bd_intf_pins axi_vdma_0/M_AXIS_MM2S]
   connect_bd_intf_net -intf_net axi_vdma_0_M_AXI_MM2S [get_bd_intf_pins axi_interconnect_0/S00_AXI] [get_bd_intf_pins axi_vdma_0/M_AXI_MM2S]
+  connect_bd_intf_net -intf_net axi_vdma_0_M_AXI_S2MM [get_bd_intf_pins axi_interconnect_0/S01_AXI] [get_bd_intf_pins axi_vdma_0/M_AXI_S2MM]
   connect_bd_intf_net -intf_net processing_system7_0_DDR [get_bd_intf_ports DDR] [get_bd_intf_pins processing_system7_0/DDR]
   connect_bd_intf_net -intf_net processing_system7_0_FIXED_IO [get_bd_intf_ports FIXED_IO] [get_bd_intf_pins processing_system7_0/FIXED_IO]
   connect_bd_intf_net -intf_net processing_system7_0_M_AXI_GP0 [get_bd_intf_pins processing_system7_0/M_AXI_GP0] [get_bd_intf_pins processing_system7_0_axi_periph/S00_AXI]
@@ -281,7 +297,7 @@ CONFIG.NUM_SI {1} \
 
   # Create port connections
   connect_bd_net -net axi_resetn_1 [get_bd_ports AXIS_RSTN] [get_bd_pins axi_interconnect_0/ARESETN] [get_bd_pins axi_interconnect_0/M00_ARESETN] [get_bd_pins axi_interconnect_0/S00_ARESETN] [get_bd_pins axi_interconnect_0/S01_ARESETN]
-  connect_bd_net -net m_axis_mm2s_aclk_1 [get_bd_ports AXIS_ACLK] [get_bd_pins axi_interconnect_0/ACLK] [get_bd_pins axi_interconnect_0/M00_ACLK] [get_bd_pins axi_interconnect_0/S00_ACLK] [get_bd_pins axi_interconnect_0/S01_ACLK] [get_bd_pins axi_vdma_0/m_axi_mm2s_aclk] [get_bd_pins axi_vdma_0/m_axis_mm2s_aclk] [get_bd_pins processing_system7_0/S_AXI_HP0_ACLK]
+  connect_bd_net -net m_axis_mm2s_aclk_1 [get_bd_ports AXIS_ACLK] [get_bd_pins axi_interconnect_0/ACLK] [get_bd_pins axi_interconnect_0/M00_ACLK] [get_bd_pins axi_interconnect_0/S00_ACLK] [get_bd_pins axi_interconnect_0/S01_ACLK] [get_bd_pins axi_vdma_0/m_axi_mm2s_aclk] [get_bd_pins axi_vdma_0/m_axi_s2mm_aclk] [get_bd_pins axi_vdma_0/m_axis_mm2s_aclk] [get_bd_pins axi_vdma_0/s_axis_s2mm_aclk] [get_bd_pins processing_system7_0/S_AXI_HP0_ACLK]
   connect_bd_net -net processing_system7_0_FCLK_CLK0 [get_bd_ports CLK_100M] [get_bd_pins axi_bram_ctrl_0/s_axi_aclk] [get_bd_pins axi_vdma_0/s_axi_lite_aclk] [get_bd_pins processing_system7_0/FCLK_CLK0] [get_bd_pins processing_system7_0/M_AXI_GP0_ACLK] [get_bd_pins processing_system7_0_axi_periph/ACLK] [get_bd_pins processing_system7_0_axi_periph/M00_ACLK] [get_bd_pins processing_system7_0_axi_periph/M01_ACLK] [get_bd_pins processing_system7_0_axi_periph/S00_ACLK] [get_bd_pins rst_processing_system7_0_50M/slowest_sync_clk]
   connect_bd_net -net processing_system7_0_FCLK_CLK1 [get_bd_ports CLK_24M] [get_bd_pins processing_system7_0/FCLK_CLK1]
   connect_bd_net -net processing_system7_0_FCLK_RESET0_N [get_bd_pins processing_system7_0/FCLK_RESET0_N] [get_bd_pins rst_processing_system7_0_50M/ext_reset_in]
@@ -290,6 +306,7 @@ CONFIG.NUM_SI {1} \
 
   # Create address segments
   create_bd_addr_seg -range 0x40000000 -offset 0x0 [get_bd_addr_spaces axi_vdma_0/Data_MM2S] [get_bd_addr_segs processing_system7_0/S_AXI_HP0/HP0_DDR_LOWOCM] SEG_processing_system7_0_HP0_DDR_LOWOCM
+  create_bd_addr_seg -range 0x40000000 -offset 0x0 [get_bd_addr_spaces axi_vdma_0/Data_S2MM] [get_bd_addr_segs processing_system7_0/S_AXI_HP0/HP0_DDR_LOWOCM] SEG_processing_system7_0_HP0_DDR_LOWOCM
   create_bd_addr_seg -range 0x2000 -offset 0x41000000 [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs axi_bram_ctrl_0/S_AXI/Mem0] SEG_axi_bram_ctrl_0_Mem0
   create_bd_addr_seg -range 0x10000 -offset 0x41100000 [get_bd_addr_spaces processing_system7_0/Data] [get_bd_addr_segs axi_vdma_0/S_AXI_LITE/Reg] SEG_axi_vdma_0_Reg
 
@@ -297,37 +314,40 @@ CONFIG.NUM_SI {1} \
   regenerate_bd_layout -layout_string {
    guistr: "# # String gsaved with Nlview 6.5.5  2015-06-26 bk=1.3371 VDI=38 GEI=35 GUI=JA:1.6
 #  -string -flagsOSRD
-preplace port DDR -pg 1 -y 290 -defaultsOSRD
-preplace port CLK_100M -pg 1 -y 350 -defaultsOSRD
-preplace port CLK_24M -pg 1 -y 370 -defaultsOSRD
+preplace port DDR -pg 1 -y 430 -defaultsOSRD
+preplace port CLK_100M -pg 1 -y 510 -defaultsOSRD
+preplace port CLK_24M -pg 1 -y 530 -defaultsOSRD
 preplace port AXIS_RSTN -pg 1 -y 140 -defaultsOSRD
 preplace port AXIS_ACLK -pg 1 -y 120 -defaultsOSRD
-preplace port FIXED_IO -pg 1 -y 310 -defaultsOSRD
-preplace port REG_CTRL -pg 1 -y 610 -defaultsOSRD
+preplace port FIXED_IO -pg 1 -y 450 -defaultsOSRD
+preplace port AXIS_S2MM -pg 1 -y 320 -defaultsOSRD
+preplace port REG_CTRL -pg 1 -y 310 -defaultsOSRD
 preplace port AXIS_MM2S -pg 1 -y 120 -defaultsOSRD
-preplace inst axi_vdma_0 -pg 1 -lvl 3 -y 140 -defaultsOSRD
-preplace inst rst_processing_system7_0_50M -pg 1 -lvl 1 -y 410 -defaultsOSRD
+preplace inst axi_vdma_0 -pg 1 -lvl 3 -y 120 -defaultsOSRD
+preplace inst rst_processing_system7_0_50M -pg 1 -lvl 1 -y 550 -defaultsOSRD
 preplace inst axi_interconnect_0 -pg 1 -lvl 2 -y 170 -defaultsOSRD
-preplace inst axi_bram_ctrl_0 -pg 1 -lvl 3 -y 610 -defaultsOSRD
-preplace inst processing_system7_0_axi_periph -pg 1 -lvl 2 -y 600 -defaultsOSRD
-preplace inst processing_system7_0 -pg 1 -lvl 3 -y 340 -defaultsOSRD
+preplace inst axi_bram_ctrl_0 -pg 1 -lvl 3 -y 310 -defaultsOSRD
+preplace inst processing_system7_0_axi_periph -pg 1 -lvl 2 -y 520 -defaultsOSRD
+preplace inst processing_system7_0 -pg 1 -lvl 3 -y 490 -defaultsOSRD
 preplace netloc processing_system7_0_DDR 1 3 1 NJ
-preplace netloc processing_system7_0_axi_periph_M00_AXI 1 2 1 N
-preplace netloc rst_processing_system7_0_50M_interconnect_aresetn 1 1 1 290
+preplace netloc processing_system7_0_axi_periph_M00_AXI 1 2 1 630
+preplace netloc rst_processing_system7_0_50M_interconnect_aresetn 1 1 1 300
 preplace netloc axi_bram_ctrl_0_BRAM_PORTA 1 3 1 NJ
-preplace netloc processing_system7_0_M_AXI_GP0 1 1 3 320 460 NJ 460 1140
-preplace netloc axi_vdma_0_M_AXI_MM2S 1 1 3 320 20 NJ 20 1160
+preplace netloc processing_system7_0_M_AXI_GP0 1 1 3 300 380 NJ 380 1060
+preplace netloc axi_vdma_0_M_AXI_MM2S 1 1 3 280 -10 NJ -10 1070
 preplace netloc axi_vdma_0_M_AXIS_MM2S 1 3 1 NJ
-preplace netloc processing_system7_0_FCLK_RESET0_N 1 0 4 -40 500 NJ 450 NJ 450 1130
-preplace netloc m_axis_mm2s_aclk_1 1 0 3 NJ 120 310 10 690
-preplace netloc rst_processing_system7_0_50M_peripheral_aresetn 1 1 2 300 440 710
+preplace netloc processing_system7_0_FCLK_RESET0_N 1 0 4 -40 670 NJ 670 NJ 670 1060
+preplace netloc m_axis_mm2s_aclk_1 1 0 3 NJ 120 300 320 640
+preplace netloc rst_processing_system7_0_50M_peripheral_aresetn 1 1 2 280 330 650
+preplace netloc S_AXIS_S2MM_1 1 0 3 NJ 20 NJ 20 NJ
 preplace netloc processing_system7_0_FIXED_IO 1 3 1 NJ
-preplace netloc axi_interconnect_0_M00_AXI 1 2 1 680
-preplace netloc axi_resetn_1 1 0 2 NJ 140 290
-preplace netloc processing_system7_0_FCLK_CLK0 1 0 4 -50 540 280 740 700 230 1150
+preplace netloc axi_interconnect_0_M00_AXI 1 2 1 610
+preplace netloc axi_resetn_1 1 0 2 NJ 140 280
+preplace netloc processing_system7_0_FCLK_CLK0 1 0 4 -50 640 290 660 620 600 1070
+preplace netloc axi_vdma_0_M_AXI_S2MM 1 1 3 300 0 NJ 0 1060
 preplace netloc processing_system7_0_FCLK_CLK1 1 3 1 NJ
-preplace netloc processing_system7_0_axi_periph_M01_AXI 1 2 1 670
-levelinfo -pg 1 -70 120 530 933 1180 -top 0 -bot 750
+preplace netloc processing_system7_0_axi_periph_M01_AXI 1 2 1 600
+levelinfo -pg 1 -70 120 460 870 1090 -top -20 -bot 680
 ",
 }
 
